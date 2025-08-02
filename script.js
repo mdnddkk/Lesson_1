35ea4cf3bf710d2e189e4556ef1ceb2e9f0c7522
Ilyas018 <karabaevilyas07@gmail.com>
2025-08-02 19:45:20 +0600

    first

--- script.js
+++ script.js
@@ -10,30 +10,66 @@ const CyanBtn = document.getElementById("Cyan")
 
 function turnOnRed () {
     leftCircleSpan.style.backgroundColor = "red"
+    RedBtn.style.backgroundColor = "green"
+    RedBtn.style.color = "white"
+    YellowBtn.style.backgroundColor = "black"
+    YellowBtn.style.color = "yellow"
+    GreenBtn.style.backgroundColor = "black"
+    GreenBtn.style.color = "green"
 }       
 RedBtn.addEventListener("click", turnOnRed)
 
 function turnOnYellow () {
     leftCircleSpan.style.backgroundColor = "yellow"
+    YellowBtn.style.backgroundColor = "green"
+    YellowBtn.style.color = "white"
+    RedBtn.style.backgroundColor = "black"
+    RedBtn.style.color = "red"
+    GreenBtn.style.backgroundColor = "black"
+    GreenBtn.style.color = "green"
 }       
 YellowBtn.addEventListener("click", turnOnYellow)
 
 function turnOnGreen () {
     leftCircleSpan.style.backgroundColor = "green"
+    GreenBtn.style.backgroundColor = "green"
+    GreenBtn.style.color = "white"
+    RedBtn.style.backgroundColor = "black"
+    RedBtn.style.color = "red"
+    YellowBtn.style.backgroundColor = "black"
+    YellowBtn.style.color = "yellow"
 }       
 GreenBtn.addEventListener("click", turnOnGreen)
 
 function turnOnMagenta () {
     rightCircleSpan.style.backgroundColor = "magenta"
+    MagentaBtn.style.backgroundColor = "green"
+    MagentaBtn.style.color = "white"
+    BlueBtn.style.backgroundColor = "black"
+    BlueBtn.style.color = "blue"
+    CyanBtn.style.backgroundColor = "black"
+    CyanBtn.style.color = "cyan"
 }
 MagentaBtn.addEventListener("click", turnOnMagenta)
 
 function turnOnBlue () {
     rightCircleSpan.style.backgroundColor = "blue"
+    BlueBtn.style.backgroundColor = "green"
+    BlueBtn.style.color = "white"
+    MagentaBtn.style.backgroundColor = "black"
+    MagentaBtn.style.color = "magenta"
+    CyanBtn.style.backgroundColor = "black"
+    CyanBtn.style.color = "cyan"
 }
 BlueBtn.addEventListener("click", turnOnBlue)
 
 function turnOnCyan () {
     rightCircleSpan.style.backgroundColor = "cyan"
+    CyanBtn.style.backgroundColor = "green"
+    CyanBtn.style.color = "white"
+    MagentaBtn.style.backgroundColor = "black"
+    MagentaBtn.style.color = "magenta"
+    BlueBtn.style.backgroundColor = "black"
+    BlueBtn.style.color = "blue"
 }
 CyanBtn.addEventListener("click", turnOnCyan)
